9191925864bde88291fe3c12cebcde1267918c94
chavindu weerakoon <chavinduweerakoon@gmail.com>
2021-08-10 18:44:31 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+//export PORT = 5000 if any - to configure env
+const port = process.env.port || 3000;
+
+//to prase json objects
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+  { id: 4, name: "course4" },
+];
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/demo/courses", (req, res) => {
+  res.send(courses);
+});
+
+//adding custom routes
+app.get("/demo/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("The couse with the given ID is not found");
+  } else {
+    res.send(course);
+  }
+});
+
+//post
+app.post("/api/demo/courses", (req, res) => {
+  if (!req.body.name || req.body.name.length < 3) {
+    //400 bad request
+    res.status(400).send("Name is required and should be minimum 3 charcters");
+    return;
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.listen(port, () => console.log(`listening on port ${port}..`));
