a392747e7945a6ce9eeac1693ec93c4d40b3d6e4
chavindu weerakoon <chavinduweerakoon@gmail.com>
2021-08-12 20:52:23 +0530

    Added data model and schema

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,15 @@
 const express = require("express");
-
 const router = express.Router();
+const Post = require("../models/Post");
 
+//get
 router.get("/", (req, res) => {
   res.send("we are on posts");
+});
+
+//post
+router.post("/", (req, res) => {
+  console.log(req.body);
 });
 
 module.exports = router;
